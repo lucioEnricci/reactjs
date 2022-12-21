bd0e7b3620408926073e5cf6e7a9f07487d6934b
unknown <lucioenricci10@gmail.com>
2022-12-21 20:17:53 +0100

    creando app de react y modificando app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <nav className="nav">
+        <a>Home</a>
+        <a>El proyecto</a>
+        <a>Nuestro objetivo</a>
+        <a>Voluntarios</a>
+        <a>Dona!</a>
+        </nav>
       </header>
+      <main className="main">
+        <h1> Contenido central</h1>
+
+
+      </main>
+      <footer className="footer">
+        <p>Seguinos en redes</p>
+        <a>Facebook</a>
+        <a>Instagram</a>
+        <p>Contactanos</p>
+        <a>Nosotros</a>
+        <a>Contacto</a>
+        <a>Voluntarios</a>
+        <p>Copyright 2022</p>
+        <p>Creado por Lucio, mis otros proyectos <a>Proyectos</a></p>
+      </footer>
     </div>
-  );
+  )
 }
 
 export default App;
